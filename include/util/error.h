63b7d68a6fee3f2860a5510777645a20954cdd60
Zhi Wang <oukore@gmail.com>
2019-04-26 18:14:17 -0500

    Error handling

---
+++ include/util/error.h
@@ -0,0 +1,32 @@
+#ifndef TINKER_UTIL_ERROR_H_
+#define TINKER_UTIL_ERROR_H_
+
+#include "macro.h"
+#include <stdexcept>
+#include <string>
+
+TINKER_NAMESPACE_BEGIN
+class FatalError : public std::exception {
+private:
+  std::string msg_;
+
+public:
+  FatalError(const char*);
+  FatalError(const std::string&);
+  FatalError(const FatalError&);
+  const char* what() const noexcept override;
+};
+
+#define m_tinker_throw(_msg)                                                   \
+  do {                                                                         \
+    std::string m__;                                                           \
+    m__ += _msg;                                                               \
+    m__ += " at ";                                                             \
+    m__ += __FILE__;                                                           \
+    m__ += ":";                                                                \
+    m__ += __LINE__;                                                           \
+    throw FatalError(m__);                                                     \
+  } while (0)
+TINKER_NAMESPACE_END
+
+#endif
