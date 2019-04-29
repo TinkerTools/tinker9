151073ed871c28b6cbd37de5d0fd7bf227532d50
Zhi Wang <oukore@gmail.com>
2019-04-29 15:32:33 -0500

    nextarg

---
+++ ext/ext/tinker.rt/tinker.rt.cpp
@@ -0,0 +1,22 @@
+#include "tinker.rt.h"
+#include "ext/tinker.mod/tinker.mod.h"
+
+TINKER_NAMESPACE_BEGIN
+void nextarg(size_t _len, char* _str, logical& _exist) {
+  const char blank = ' ';
+  std::memset(_str, blank, _len);
+  _exist = _false_;
+
+  if (argue::narg != 0) {
+    size_t length = std::min(_len, sizeof(argue::arg[argue::maxarg]));
+    for (size_t i = 1; i <= argue::narg; ++i) {
+      if (argue::listarg[i]) {
+        argue::listarg[i] = _false_;
+        std::strncpy(_str, argue::arg[i], length);
+        _exist = _true_;
+        break;
+      }
+    }
+  }
+}
+TINKER_NAMESPACE_END
---
+++ ext/ext/tinker.rt/tinker.rt.h
@@ -0,0 +1,27 @@
+#ifndef TINKER_RT_H_
+#define TINKER_RT_H_
+
+#include "util/cxx.h"
+
+TINKER_NAMESPACE_BEGIN
+typedef int logical;
+const logical _true_ = 1;
+const logical _false_ = 0;
+
+void nextarg(size_t _len, char* _str, logical& _exist);
+
+template <size_t _Len>
+void nextarg(char (&_str)[_Len], logical& _exist) {
+  nextarg(_Len, _str, _exist);
+}
+TINKER_NAMESPACE_END
+
+extern "C" {
+void TINKER_RT(final)();
+void TINKER_RT(getxyz)();
+void TINKER_RT(initial)();
+void TINKER_RT(mdinit)();
+void TINKER_RT(mechanic)();
+}
+
+#endif
