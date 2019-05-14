f2801d9106a57ff432774bfcbfa5b8b5af59f7a4
Zhi Wang <oukore@gmail.com>
2019-05-14 16:25:22 -0500

    Add CLN025 ehal gradient tests

--- src/test/test_cln025.cpp
+++ src/test/test_cln025.cpp
@@ -28,9 +28,14 @@ TEST_CASE("Ehal-CLN025", "[forcefield][ehal][cln025]") {
     const char* argv[] = {"dummy", x};
     int argc = 2;
 
-    const double eps = 1.0e-4;
+    const double eps = 2.0e-4;
     const double ref_eng = 88.8860;
     const int ref_count = 13225;
+    // atom 1, 31, 61, 91, 121, 151
+    const double ref_g[][3] = {
+        {-10.4202, 0.6937, 2.7019}, {4.0910, 2.0984, -2.4349},
+        {-0.5266, 3.5665, 0.4037},  {-0.2577, -1.9100, 6.9858},
+        {1.0470, 3.2855, -4.3019},  {4.3475, -2.2594, 1.8280}};
     const double ref_v[][3] = {{-800.488, -37.589, -2.250},
                                {-37.589, -758.657, 41.895},
                                {-2.250, 41.895, -681.179}};
@@ -48,6 +53,19 @@ TEST_CASE("Ehal-CLN025", "[forcefield][ehal][cln025]") {
   {                                                                            \
     int count = count = gpu::count_evdw();                                     \
     REQUIRE(count == ref_count);                                               \
+  }
+#define COMPARE_GRAD_                                                          \
+  {                                                                            \
+    grad_t grad(gpu::n);                                                       \
+    double* dst = &grad[0][0];                                                 \
+    gpu::copyout_data2(0, 3, dst, gpu::gx, gpu::n);                            \
+    gpu::copyout_data2(1, 3, dst, gpu::gy, gpu::n);                            \
+    gpu::copyout_data2(2, 3, dst, gpu::gz, gpu::n);                            \
+    for (int i = 0; i < 6; ++i) {                                              \
+      for (int j = 0; j < 3; ++j) {                                            \
+        REQUIRE(grad[i * 30][j] == Approx(ref_g[i][j]).epsilon(eps));          \
+      }                                                                        \
+    }                                                                          \
   }
 #define COMPARE_VIR_                                                           \
   {                                                                            \
@@ -69,7 +87,7 @@ TEST_CASE("Ehal-CLN025", "[forcefield][ehal][cln025]") {
     gpu::zero_egv();                                                           \
     tinker_gpu_evdw_hal1();                                                    \
     COMPARE_ENERGY_;                                                           \
-    /* COMPARE_GRAD_; */                                                       \
+    COMPARE_GRAD_;                                                             \
     COMPARE_VIR_;                                                              \
                                                                                \
     gpu::zero_egv();                                                           \
@@ -80,15 +98,15 @@ TEST_CASE("Ehal-CLN025", "[forcefield][ehal][cln025]") {
     gpu::zero_egv();                                                           \
     tinker_gpu_evdw_hal4();                                                    \
     COMPARE_ENERGY_;                                                           \
-    /* COMPARE_GRAD_; */                                                       \
+    COMPARE_GRAD_;                                                             \
                                                                                \
     gpu::zero_egv();                                                           \
     tinker_gpu_evdw_hal5();                                                    \
-    /* COMPARE_GRAD_; */                                                       \
+    COMPARE_GRAD_;                                                             \
                                                                                \
     gpu::zero_egv();                                                           \
     tinker_gpu_evdw_hal6();                                                    \
-    /* COMPARE_GRAD_; */                                                       \
+    COMPARE_GRAD_;                                                             \
     COMPARE_VIR_;                                                              \
   }
 
@@ -108,9 +126,14 @@ TEST_CASE("Ehal-CLN025", "[forcefield][ehal][cln025]") {
     const char* argv[] = {"dummy", x};
     int argc = 2;
 
-    const double eps = 1.0e-4;
+    const double eps = 2.0e-4;
     const double ref_eng = 104.3872;
     const int ref_count = 4116;
+    // atom 1, 31, 61, 91, 121, 151
+    const double ref_g[][3] = {
+        {-10.5674, 0.6411, 2.6721}, {4.0652, 2.1284, -2.4015},
+        {-0.5214, 3.5720, 0.3980},  {-0.1419, -1.9235, 6.9387},
+        {1.0707, 3.2989, -4.2483},  {4.3235, -2.2325, 1.8485}};
     const double ref_v[][3] = {{-914.956, -8.336, 26.361},
                                {-8.336, -781.555, 29.820},
                                {26.361, 29.820, -706.064}};
