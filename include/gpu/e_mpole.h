6994c3e80c130fe6ed45bb81033c5f11e6100c17
Zhi Wang <oukore@gmail.com>
2019-05-14 18:42:21 -0500

    Pass polaxe etc. to GPU

--- include/gpu/e_mpole.h
+++ include/gpu/e_mpole.h
@@ -10,6 +10,12 @@ const int elec_ewald = 2;
 extern int electyp;
 extern std::string electyp_str;
 
+const int pole_none = 0;
+const int pole_z_only = 1;
+const int pole_z_then_x = 2;
+const int pole_bisector = 3;
+const int pole_z_bisect = 4;
+const int pole_3_fold = 5;
 struct local_frame_def_st {
   int zaxis, xaxis, yaxis;
   int polaxe;
@@ -30,12 +36,16 @@ const int mpl_qzx = mpl_qxz;
 const int mpl_qzy = mpl_qyz;
 const int mpl_qzz = 9;
 const int mpl_total = 10;
+// (1), (2, 3, 4), (5, 6, 7, 9, 10, 13)
+const int mpl_tinker[mpl_total] = {0, 1, 2, 3, 4, 5, 6, 8, 9, 12};
 extern real (*pole)[mpl_total];
 extern real (*rpole)[mpl_total];
 
 extern real* em;
 extern int* nem;
 extern real* vir_em;
+extern real* torque;
+
 int use_empole();
 void get_empole_type(int& typ, std::string& typ_str);
 void e_mpole_data(int op);
--- src/gpu/rc_mpole.cpp
+++ src/gpu/rc_mpole.cpp
@@ -3,6 +3,7 @@
 #include "gpu/decl_nblist.h"
 #include "gpu/e_mpole.h"
 #include "rc_cudart.h"
+#include "util/fort_str.h"
 
 TINKER_NAMESPACE_BEGIN
 namespace gpu {
@@ -17,6 +18,8 @@ real (*rpole)[mpl_total];
 real* em;
 int* nem;
 real* vir_em;
+real* torque;
+
 int use_empole() { return potent::use_mpole; }
 
 void get_empole_type(int& typ, std::string& typ_str) {
@@ -60,6 +63,7 @@ void e_mpole_data(int op) {
     check_cudart(cudaFree(em));
     check_cudart(cudaFree(nem));
     check_cudart(cudaFree(vir_em));
+    check_cudart(cudaFree(torque));
   }
 
   if (op == op_create) {
@@ -70,13 +74,47 @@ void e_mpole_data(int op) {
 
     size = sizeof(local_frame_def_st);
     check_cudart(cudaMalloc(&zaxis, n * size));
+    std::vector<int> zaxisbuf(4 * n);
+    for (int i = 0; i < n; ++i) {
+      int base = 4 * i;
+      zaxisbuf[base] = mpole::zaxis[i] - 1;
+      zaxisbuf[base + 1] = mpole::xaxis[i] - 1;
+      zaxisbuf[base + 2] = mpole::yaxis[i] - 1;
+      fstr_view str = mpole::polaxe[i];
+      int val;
+      if (str == "Z-Only")
+        val = pole_z_only;
+      else if (str == "Z-then-X")
+        val = pole_z_then_x;
+      else if (str == "Bisector")
+        val = pole_bisector;
+      else if (str == "Z-Bisect")
+        val = pole_z_bisect;
+      else if (str == "3-Fold")
+        val = pole_3_fold;
+      else
+        val = pole_none;
+      zaxisbuf[base + 3] = val;
+    }
+    copyin_data(reinterpret_cast<int*>(zaxis), zaxisbuf.data(), 4 * n);
+
     size = rs * mpl_total;
     check_cudart(cudaMalloc(&pole, n * size));
     check_cudart(cudaMalloc(&rpole, n * size));
+    std::vector<double> polebuf(mpl_total * n);
+    for (int i = 0; i < n; ++i) {
+      int b1 = mpl_total * i;
+      int b2 = mpole::maxpole * i;
+      for (int j = 0; j < mpl_total; ++j)
+        polebuf[b1 + j] = mpole::pole[b2 + mpl_tinker[j]];
+    }
+    copyin_data(reinterpret_cast<real*>(pole), polebuf.data(), mpl_total * n);
 
     check_cudart(cudaMalloc(&em, rs));
     check_cudart(cudaMalloc(&nem, sizeof(int)));
     check_cudart(cudaMalloc(&vir_em, 9 * rs));
+    if (use_data & use_grad)
+      check_cudart(cudaMalloc(&torque, 3 * rs * n));
   }
 }
 }
