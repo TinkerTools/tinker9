dc7ec035c4dc8bee10e53dfe1989effcbf6bb36e
Zhi Wang <oukore@gmail.com>
2019-05-19 23:13:59 -0500

    More cleaning in acc_torque

--- src/gpu/acc_torque.cc
+++ src/gpu/acc_torque.cc
@@ -1,24 +1,23 @@
 #include "acc_e.h"
 #include "gpu/e_mpole.h"
 
-#define SET_ARRAY3_(a, ia, ib)                                                 \
-  a[0] = x[ia] - x[ib];                                                        \
-  a[1] = y[ia] - y[ib];                                                        \
-  a[2] = z[ia] - z[ib]
-#define NORM_(a) REAL_SQRT(a[0] * a[0] + a[1] * a[1] + a[2] * a[2])
-#define CROSS_(ans, u, v)                                                      \
-  ans[0] = u[1] * v[2] - u[2] * v[1];                                          \
-  ans[1] = u[2] * v[0] - u[0] * v[2];                                          \
-  ans[2] = u[0] * v[1] - u[1] * v[0]
-#define NORMALIZE_(a, _1_na)                                                   \
-  a[0] *= _1_na;                                                               \
-  a[1] *= _1_na;                                                               \
-  a[2] *= _1_na
 #define ADD_(ans, a, b)                                                        \
   ans[0] = a[0] + b[0];                                                        \
   ans[1] = a[1] + b[1];                                                        \
   ans[2] = a[2] + b[2]
+#define SUB_(a, ia, ib)                                                        \
+  a[0] = x[ia] - x[ib];                                                        \
+  a[1] = y[ia] - y[ib];                                                        \
+  a[2] = z[ia] - z[ib]
 #define DOT_(a, b) (a[0] * b[0] + a[1] * b[1] + a[2] * b[2])
+#define CROSS_(ans, u, v)                                                      \
+  ans[0] = u[1] * v[2] - u[2] * v[1];                                          \
+  ans[1] = u[2] * v[0] - u[0] * v[2];                                          \
+  ans[2] = u[0] * v[1] - u[1] * v[0]
+#define NORMAL_(a, _1_na)                                                      \
+  a[0] *= _1_na;                                                               \
+  a[1] *= _1_na;                                                               \
+  a[2] *= _1_na
 
 TINKER_NAMESPACE_BEGIN
 namespace gpu {
@@ -29,15 +28,18 @@ void torque_tmpl(real* gpu_vir) {
                              gpu_vir)
   #pragma acc parallel loop
   for (int i = 0; i < n; ++i) {
-    real frcz[3] = {0, 0, 0};
-    real frcx[3] = {0, 0, 0};
-    real frcy[3] = {0, 0, 0};
     const int axetyp = zaxis[i].polaxe;
     if (axetyp == pole_none)
       continue;
 
     const real trq[3] = {trqx[i], trqy[i], trqz[i]};
     real* de[3] = {gx, gy, gz};
+
+    // zero out force components on local frame-defining atoms
+
+    real frcz[3] = {0, 0, 0};
+    real frcx[3] = {0, 0, 0};
+    real frcy[3] = {0, 0, 0};
 
     // get the local frame type and the frame-defining atoms
 
@@ -48,12 +50,13 @@ void torque_tmpl(real* gpu_vir) {
 
     // construct the three rotation axes for the local frame
 
-    real u[3], v[3], w[3];
-    real usiz1, vsiz1, wsiz1;
-    SET_ARRAY3_(u, ia, ib);
+    real u[3], usiz1;
+    SUB_(u, ia, ib);
     usiz1 = REAL_RSQRT(DOT_(u, u));
+
+    real v[3], vsiz1;
     if (axetyp != pole_z_only) {
-      SET_ARRAY3_(v, ic, ib);
+      SUB_(v, ic, ib);
       vsiz1 = REAL_RSQRT(DOT_(v, v));
     } else {
       if (REAL_ABS(u[0]) * usiz1 > ((real)0.866)) {
@@ -66,111 +69,40 @@ void torque_tmpl(real* gpu_vir) {
       v[2] = 0;
       vsiz1 = 1;
     }
+
+    real w[3], wsiz1;
     if (axetyp == pole_z_bisect || axetyp == pole_3_fold) {
-      SET_ARRAY3_(w, id, ib);
+      SUB_(w, id, ib);
     } else {
       CROSS_(w, u, v);
     }
     wsiz1 = REAL_RSQRT(DOT_(w, w));
-    NORMALIZE_(u, usiz1);
-    NORMALIZE_(v, vsiz1);
-    NORMALIZE_(w, wsiz1);
 
-    // build some additional axes needed for the Z-Bisect method
-
-    real r[3], s[3];
-    real rsiz1, ssiz1;
-    if (axetyp == pole_z_bisect) {
-      ADD_(r, v, w);
-      rsiz1 = REAL_RSQRT(DOT_(r, r));
-      CROSS_(s, u, r);
-      ssiz1 = REAL_RSQRT(DOT_(s, s));
-      NORMALIZE_(r, rsiz1);
-      NORMALIZE_(s, ssiz1);
-    }
+    NORMAL_(u, usiz1);
+    NORMAL_(v, vsiz1);
+    NORMAL_(w, wsiz1);
 
     // find the perpendicular and angle for each pair of axes
 
-    real uv[3], uw[3], vw[3];
-    real uvsiz1, uwsiz1, vwsiz1;
-
+    real uv[3], uvsiz1;
     CROSS_(uv, v, u);
     uvsiz1 = REAL_RSQRT(DOT_(uv, uv));
-    NORMALIZE_(uv, uvsiz1);
+    NORMAL_(uv, uvsiz1);
 
+    real uw[3], uwsiz1;
     CROSS_(uw, w, u);
     uwsiz1 = REAL_RSQRT(DOT_(uw, uw));
-    NORMALIZE_(uw, uwsiz1);
+    NORMAL_(uw, uwsiz1);
 
+    real vw[3], vwsiz1;
     CROSS_(vw, w, v);
     vwsiz1 = REAL_RSQRT(DOT_(vw, vw));
-    NORMALIZE_(vw, vwsiz1);
-
-    real ur[3], us[3], vs[3], ws[3];
-    real ursiz1, ussiz1, vssiz1, wssiz1;
-    if (axetyp == pole_z_bisect) {
-      CROSS_(ur, r, u);
-      ursiz1 = REAL_RSQRT(DOT_(ur, ur));
-      NORMALIZE_(ur, ursiz1);
-
-      CROSS_(us, s, u);
-      ussiz1 = REAL_RSQRT(DOT_(us, us));
-      NORMALIZE_(us, ussiz1);
-
-      CROSS_(vs, s, v);
-      vssiz1 = REAL_RSQRT(DOT_(vs, vs));
-      NORMALIZE_(vs, vssiz1);
-
-      CROSS_(ws, s, w);
-      wssiz1 = REAL_RSQRT(DOT_(ws, ws));
-      NORMALIZE_(ws, wssiz1);
-    }
+    NORMAL_(vw, vwsiz1);
 
     // get sine and cosine of angles between the rotation axes
 
     real uvcos = DOT_(u, v);
-    real uvsin = REAL_SQRT(1 - uvcos * uvcos);
-    real uvsin1 = REAL_RECIP(uvsin);
-    real uwcos = DOT_(u, w);
-    real uwsin = REAL_SQRT(1 - uwcos * uwcos);
-    real vwcos = DOT_(v, w);
-    real vwsin = REAL_SQRT(1 - vwcos * vwcos);
-
-    real urcos, ursin1;
-    real vscos, vssin;
-    real wscos, wssin;
-    if (axetyp == pole_z_bisect) {
-      urcos = DOT_(u, r);
-      ursin1 = REAL_RSQRT(1 - urcos * urcos);
-      vscos = DOT_(v, s);
-      vssin = REAL_SQRT(1 - vscos * vscos);
-      wscos = DOT_(w, s);
-      wssin = REAL_SQRT(1 - wscos * wscos);
-    }
-
-    // compute the projection of v and w onto the ru-plane
-
-    real t1[3], t2[3];
-    real t1siz1, t2siz1;
-    real _1_ut1sin_ut2sin;
-    if (axetyp == pole_z_bisect) {
-      t1[0] = v[0] - s[0] * vscos;
-      t1[1] = v[1] - s[1] * vscos;
-      t1[2] = v[2] - s[2] * vscos;
-      t1siz1 = REAL_RSQRT(DOT_(t1, t1));
-      NORMALIZE_(t1, t1siz1);
-      t2[0] = w[0] - s[0] * wscos;
-      t2[1] = w[1] - s[1] * wscos;
-      t2[2] = w[2] - s[2] * wscos;
-      t2siz1 = REAL_RSQRT(DOT_(t2, t2));
-      NORMALIZE_(t2, t2siz1);
-
-      real ut1cos = DOT_(u, t1);
-      real ut1sin = REAL_SQRT(1 - ut1cos * ut1cos);
-      real ut2cos = DOT_(u, t2);
-      real ut2sin = REAL_SQRT(1 - ut2cos * ut2cos);
-      _1_ut1sin_ut2sin = REAL_RECIP(ut1sin + ut2sin);
-    }
+    real uvsin1 = REAL_RSQRT(1 - uvcos * uvcos);
 
     // negative of dot product of torque with unit vectors gives result of
     // infinitesimal rotation along these vectors
@@ -178,13 +110,9 @@ void torque_tmpl(real* gpu_vir) {
     real dphidu = -DOT_(trq, u);
     real dphidv = -DOT_(trq, v);
     real dphidw = -DOT_(trq, w);
-    real dphidr, dphids;
-    if (axetyp == pole_z_bisect) {
-      dphidr = -DOT_(trq, r);
-      dphids = -DOT_(trq, s);
-    }
 
     // force distribution
+
     if (axetyp == pole_z_only) {
       for (int j = 0; j < 3; ++j) {
         real du = uv[j] * dphidv * usiz1 * uvsin1 + uw[j] * dphidw * usiz1;
@@ -192,7 +120,7 @@ void torque_tmpl(real* gpu_vir) {
         de[j][ia] += du;
         #pragma acc atomic update
         de[j][ib] -= du;
-        frcz[j] += du;
+        if_constexpr(DO_V) frcz[j] += du;
       }
     } else if (axetyp == pole_z_then_x) {
       for (int j = 0; j < 3; ++j) {
@@ -204,8 +132,10 @@ void torque_tmpl(real* gpu_vir) {
         de[j][ic] += dv;
         #pragma acc atomic update
         de[j][ib] -= (du + dv);
-        frcz[j] += du;
-        frcx[j] += dv;
+        if_constexpr(DO_V) {
+          frcz[j] += du;
+          frcx[j] += dv;
+        }
       }
     } else if (axetyp == pole_bisector) {
       for (int j = 0; j < 3; ++j) {
@@ -219,10 +149,86 @@ void torque_tmpl(real* gpu_vir) {
         de[j][ic] += dv;
         #pragma acc atomic update
         de[j][ib] -= (du + dv);
-        frcz[j] += du;
-        frcx[j] += dv;
+        if_constexpr(DO_V) {
+          frcz[j] += du;
+          frcx[j] += dv;
+        }
       }
     } else if (axetyp == pole_z_bisect) {
+
+      // build some additional axes needed for the Z-Bisect method
+
+      real r[3], rsiz1;
+      ADD_(r, v, w);
+      rsiz1 = REAL_RSQRT(DOT_(r, r));
+
+      real s[3], ssiz1;
+      CROSS_(s, u, r);
+      ssiz1 = REAL_RSQRT(DOT_(s, s));
+
+      NORMAL_(r, rsiz1);
+      NORMAL_(s, ssiz1);
+
+      // find the perpendicular and angle for each pair of axes
+
+      real ur[3], ursiz1;
+      CROSS_(ur, r, u);
+      ursiz1 = REAL_RSQRT(DOT_(ur, ur));
+      NORMAL_(ur, ursiz1);
+
+      real us[3], ussiz1;
+      CROSS_(us, s, u);
+      ussiz1 = REAL_RSQRT(DOT_(us, us));
+      NORMAL_(us, ussiz1);
+
+      real vs[3], vssiz1;
+      CROSS_(vs, s, v);
+      vssiz1 = REAL_RSQRT(DOT_(vs, vs));
+      NORMAL_(vs, vssiz1);
+
+      real ws[3], wssiz1;
+      CROSS_(ws, s, w);
+      wssiz1 = REAL_RSQRT(DOT_(ws, ws));
+      NORMAL_(ws, wssiz1);
+
+      // get sine and cosine of angles between the rotation axes
+
+      real urcos = DOT_(u, r);
+      real ursin1 = REAL_RSQRT(1 - urcos * urcos);
+      real vscos = DOT_(v, s);
+      real vssin = REAL_SQRT(1 - vscos * vscos);
+      real wscos = DOT_(w, s);
+      real wssin = REAL_SQRT(1 - wscos * wscos);
+
+      // compute the projection of v and w onto the ru-plane
+
+      real t1[3], t1siz1;
+      real t2[3], t2siz1;
+
+      t1[0] = v[0] - s[0] * vscos;
+      t1[1] = v[1] - s[1] * vscos;
+      t1[2] = v[2] - s[2] * vscos;
+      t1siz1 = REAL_RSQRT(DOT_(t1, t1));
+      NORMAL_(t1, t1siz1);
+
+      t2[0] = w[0] - s[0] * wscos;
+      t2[1] = w[1] - s[1] * wscos;
+      t2[2] = w[2] - s[2] * wscos;
+      t2siz1 = REAL_RSQRT(DOT_(t2, t2));
+      NORMAL_(t2, t2siz1);
+
+      real ut1cos = DOT_(u, t1);
+      real ut1sin = REAL_SQRT(1 - ut1cos * ut1cos);
+      real ut2cos = DOT_(u, t2);
+      real ut2sin = REAL_SQRT(1 - ut2cos * ut2cos);
+      real _1_ut1sin_ut2sin = REAL_RECIP(ut1sin + ut2sin);
+
+      // negative of dot product of torque with unit vectors gives result of
+      // infinitesimal rotation along these vectors
+
+      real dphidr = -DOT_(trq, r);
+      real dphids = -DOT_(trq, s);
+
       for (int j = 0; j < 3; ++j) {
         real du = ur[j] * dphidr * usiz1 * ursin1 + us[j] * dphids * usiz1;
         real dv =
@@ -237,9 +243,11 @@ void torque_tmpl(real* gpu_vir) {
         de[j][id] += dw;
         #pragma acc atomic update
         de[j][ib] -= (du + dv + dw);
-        frcz[j] += du;
-        frcx[j] += dv;
-        frcy[j] += dw;
+        if_constexpr(DO_V) {
+          frcz[j] += du;
+          frcx[j] += dv;
+          frcy[j] += dw;
+        }
       }
     } else if (axetyp == pole_3_fold) {
       real p[3], psiz1;
@@ -247,30 +255,25 @@ void torque_tmpl(real* gpu_vir) {
       p[1] = u[1] + v[1] + w[1];
       p[2] = u[2] + v[2] + w[2];
       psiz1 = REAL_RSQRT(DOT_(p, p));
-      NORMALIZE_(p, psiz1);
+      NORMAL_(p, psiz1);
 
-      real wpcos;
-      real upcos;
-      real vpcos;
-      wpcos = DOT_(w, p);
-      upcos = DOT_(u, p);
-      vpcos = DOT_(v, p);
+      real wpcos = DOT_(w, p);
+      real upcos = DOT_(u, p);
+      real vpcos = DOT_(v, p);
 
       real r[3], rsiz1;
-      real dphidr;
       real del[3], delsiz1;
-      real dphiddel;
-      real eps[3];
+      real eps[3], dphidr, dphiddel;
 
       ADD_(r, u, v);
       rsiz1 = REAL_RSQRT(DOT_(r, r));
-      NORMALIZE_(r, rsiz1);
+      NORMAL_(r, rsiz1);
       real rwcos = DOT_(r, w);
       real rwsin1 = REAL_RSQRT(1 - rwcos * rwcos);
       dphidr = -DOT_(trq, r);
       CROSS_(del, r, w);
       delsiz1 = REAL_RSQRT(DOT_(del, del));
-      NORMALIZE_(del, delsiz1);
+      NORMAL_(del, delsiz1);
       dphiddel = -DOT_(trq, del);
       CROSS_(eps, del, w);
       for (int j = 0; j < 3; ++j) {
@@ -280,18 +283,18 @@ void torque_tmpl(real* gpu_vir) {
         de[j][id] += dw;
         #pragma acc atomic update
         de[j][ib] -= dw;
-        frcy[j] += dw;
+        if_constexpr(DO_V) frcy[j] += dw;
       }
 
       ADD_(r, v, w);
       rsiz1 = REAL_RSQRT(DOT_(r, r));
-      NORMALIZE_(r, rsiz1);
+      NORMAL_(r, rsiz1);
       real rucos = DOT_(r, u);
       real rusin1 = REAL_RSQRT(1 - rucos * rucos);
       dphidr = -DOT_(trq, r);
       CROSS_(del, r, u);
       delsiz1 = REAL_RSQRT(DOT_(del, del));
-      NORMALIZE_(del, delsiz1);
+      NORMAL_(del, delsiz1);
       dphiddel = -DOT_(trq, del);
       CROSS_(eps, del, u);
       for (int j = 0; j < 3; ++j) {
@@ -301,18 +304,18 @@ void torque_tmpl(real* gpu_vir) {
         de[j][ia] += du;
         #pragma acc atomic update
         de[j][ib] -= du;
-        frcz[j] += du;
+        if_constexpr(DO_V) frcz[j] += du;
       }
 
       ADD_(r, u, w);
       rsiz1 = REAL_RSQRT(DOT_(r, r));
-      NORMALIZE_(r, rsiz1);
+      NORMAL_(r, rsiz1);
       real rvcos = DOT_(r, v);
       real rvsin1 = REAL_RSQRT(1 - rvcos * rvcos);
       dphidr = -DOT_(trq, r);
       CROSS_(del, r, v);
       delsiz1 = REAL_RSQRT(DOT_(del, del));
-      NORMALIZE_(del, delsiz1);
+      NORMAL_(del, delsiz1);
       dphiddel = -DOT_(trq, del);
       CROSS_(eps, del, v);
       for (int j = 0; j < 3; ++j) {
@@ -322,14 +325,14 @@ void torque_tmpl(real* gpu_vir) {
         de[j][ic] += dv;
         #pragma acc atomic update
         de[j][ib] -= dv;
-        frcx[j] += dv;
+        if_constexpr(DO_V) frcx[j] += dv;
       }
     }
 
     if_constexpr(DO_V) {
-      int iaz = (ia == -1) ? i : ia;
-      int iax = (ic == -1) ? i : ic;
-      int iay = (id == -1) ? i : id;
+      const int iaz = (ia == -1) ? i : ia;
+      const int iax = (ic == -1) ? i : ic;
+      const int iay = (id == -1) ? i : id;
 
       real xiz = x[iaz] - x[i];
       real yiz = y[iaz] - y[i];
@@ -387,9 +390,8 @@ void torque1() {
 }
 TINKER_NAMESPACE_END
 
-#undef SET_ARRAY3_
-#undef NORM_
-#undef CROSS_
-#undef NORMALIZE_
 #undef ADD_
+#undef SUB_
 #undef DOT_
+#undef CROSS_
+#undef NORMAL_
