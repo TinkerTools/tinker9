2c963476ce476e421133c33b2347523dee9b0994
Zhi Wang <oukore@gmail.com>
2019-05-20 03:47:06 -0500

    Oops. Missed two files.

---
+++ include/gpu/decl_pme.h
@@ -0,0 +1,47 @@
+#ifndef TINKER_GPU_DECL_PME_H_
+#define TINKER_GPU_DECL_PME_H_
+
+#include "decl_real.h"
+#include <vector>
+
+TINKER_NAMESPACE_BEGIN
+namespace gpu {
+/**
+ * pme.f and kewald.f
+ *
+ * allocate igrid(3,n)                   !! integer
+ * allocate (bsmod1(nfft1))              !! real*8
+ * allocate (bsmod2(nfft2))              !! real*8
+ * allocate (bsmod3(nfft3))              !! real*8
+ * allocate (bsbuild(bsorder,bsorder))   !! real*8
+ * allocate (thetai1(4,bsorder,n))       !! real*8
+ * allocate (thetai2(4,bsorder,n))       !! real*8
+ * allocate (thetai3(4,bsorder,n))       !! real*8
+ * allocate (qgrid(2,nfft1,nfft2,nfft3)) !! real*8
+ * allocate (qfac(nfft1,nfft2,nfft3))    !! real*8
+ */
+struct pme_st {
+  int nfft1, nfft2, nfft3, bsorder;
+  int* igrid;                                       // deviceptr
+  real *bsmod1, *bsmod2, *bsmod3, *bsbuild;         // deviceptr
+  real *thetai1, *thetai2, *thetai3, *qgrid, *qfac; // deviceptr
+};
+
+namespace detail_ {
+std::vector<pme_st>& pme_objs();
+std::vector<pme_st*>& pme_deviceptrs();
+}
+
+int pme_open_unit(int nfft1, int nfft2, int nfft3, int bsorder);
+pme_st& pme_obj(int index);
+pme_st* pme_deviceptr(int index);
+
+extern int epme_unit; // electrostatic
+extern int ppme_unit; // polarization
+extern int dpme_unit; // dispersion
+
+void pme_data(int op);
+}
+TINKER_NAMESPACE_END
+
+#endif
