70a7075b6ff074486535808a64b5faf71b675114
Zhi Wang <oukore@gmail.com>
2019-06-02 16:46:34 -0500

    Makefile

--- src/gpu/acc_vdw.cc
+++ src/gpu/acc_vdw.cc
@@ -72,198 +72,196 @@ void evdw_tmpl() {
     vscale[i] = 1;
   }
 
-  #pragma acc data deviceptr(x,y,z,gx,gy,gz,box,couple,vlst,\
-                             ired,kred,xred,yred,zred,\
-                             jvdw,njvdw,radmin,epsilon,\
-                             vlam,\
-                             ev,nev,vir_ev)
+  #pragma acc serial deviceptr(ev,nev,vir_ev)
   {
-    #pragma acc serial
-    {
-      *ev = 0;
-      if_constexpr(do_a) { *nev = 0; }
-      if_constexpr(do_v) {
-        for (int i = 0; i < 9; ++i)
-          vir_ev[i] = 0;
-      }
+    *ev = 0;
+    if_constexpr(do_a) { *nev = 0; }
+    if_constexpr(do_v) {
+      for (int i = 0; i < 9; ++i)
+        vir_ev[i] = 0;
     }
-
-    #pragma acc parallel loop firstprivate(vscale[0:n])
-    for (int i = 0; i < n; ++i) {
-      const int n12i = couple->n12[i];
-      const int n13i = couple->n13[i];
-      const int n14i = couple->n14[i];
-      const int n15i = couple->n15[i];
-      #pragma acc loop independent
-      for (int j = 0; j < n12i; ++j)
-        vscale[couple->i12[i][j]] = v2scale;
-      #pragma acc loop independent
-      for (int j = 0; j < n13i; ++j)
-        vscale[couple->i13[i][j]] = v3scale;
-      #pragma acc loop independent
-      for (int j = 0; j < n14i; ++j)
-        vscale[couple->i14[i][j]] = v4scale;
-      #pragma acc loop independent
-      for (int j = 0; j < n15i; ++j)
-        vscale[couple->i15[i][j]] = v5scale;
-
-      int iv = ired[i];
-      real redi = kred[i];
-      real rediv = 1 - redi;
-      int it = jvdw[i];
-      real xi, yi, zi;
-      xi = xred[i];
-      yi = yred[i];
-      zi = zred[i];
-      real lam1 = vlam[i];
-
-      int base_it = it * (*njvdw);
-      int nvlsti = vlst->nlst[i];
-      int base = i * maxnlst;
-      #pragma acc loop independent
-      for (int kk = 0; kk < nvlsti; ++kk) {
-        int k = vlst->lst[base + kk];
-        int kv = ired[k];
-        int kt = jvdw[k];
-        real xr, yr, zr;
-        xr = xi - xred[k];
-        yr = yi - yred[k];
-        zr = zi - zred[k];
-        real vlambda = vlam[k];
-
-        if (vcouple == vcouple_decouple) {
-          vlambda = (lam1 == vlambda ? 1 : (lam1 < vlambda ? lam1 : vlambda));
-        } else if (vcouple == vcouple_annihilate) {
-          vlambda = (lam1 < vlambda ? lam1 : vlambda);
-        }
-
-        image(xr, yr, zr, box);
-        real rik2 = xr * xr + yr * yr + zr * zr;
-        if (rik2 <= off2) {
-          real rik = REAL_SQRT(rik2);
-          real rv = radmin[base_it + kt];
-          real eps = epsilon[base_it + kt] * vscale[k];
-
-          real e;
-          real de;
-          if_constexpr(VDWTYP & evdw_hal) {
-            real rho = rik * REAL_RECIP(rv);
-            real rho6 = REAL_POW(rho, 6);
-            real rho7 = rho6 * rho;
-            eps *= REAL_POW(vlambda, scexp);
-            real scal = scalpha * REAL_POW(1 - vlambda, 2);
-            real s1 = REAL_RECIP(scal + REAL_POW(rho + dhal, 7));
-            real s2 = REAL_RECIP(scal + rho7 + ghal);
-            real t1 = REAL_POW(1 + dhal, 7) * s1;
-            real t2 = (1 + ghal) * s2;
-            e = eps * t1 * (t2 - 2);
-            if_constexpr(do_g) {
-              real dt1drho = -7 * REAL_POW(rho + dhal, 6) * t1 * s1;
-              real dt2drho = -7 * rho6 * t2 * s2;
-              de = eps * (dt1drho * (t2 - 2) + t1 * dt2drho) * REAL_RECIP(rv);
-            }
-          }
-
-          if (rik2 > cut2) {
-            real taper, dtaper;
-            switch_taper5<do_g>(rik, cut, off, taper, dtaper);
-            if_constexpr(do_g) de = e * dtaper + de * taper;
-            if_constexpr(do_e) e = e * taper;
-          }
-
-          // Increment the energy, gradient, and virial.
-
-          if_constexpr(do_e) {
-            #pragma acc atomic update
-            *ev += e;
-            if_constexpr(do_a) {
-              if (e != 0) {
-                #pragma acc atomic update
-                *nev += 1;
-              }
-            }
-          }
-
-          real dedx, dedy, dedz;
-          if_constexpr(do_g) {
-            de *= REAL_RECIP(rik);
-            dedx = de * xr;
-            dedy = de * yr;
-            dedz = de * zr;
-
-            #pragma acc atomic update
-            gx[i] += dedx * redi;
-            #pragma acc atomic update
-            gy[i] += dedy * redi;
-            #pragma acc atomic update
-            gz[i] += dedz * redi;
-            #pragma acc atomic update
-            gx[iv] += dedx * rediv;
-            #pragma acc atomic update
-            gy[iv] += dedy * rediv;
-            #pragma acc atomic update
-            gz[iv] += dedz * rediv;
-
-            real redk = kred[k];
-            real redkv = 1 - redk;
-            #pragma acc atomic update
-            gx[k] -= dedx * redk;
-            #pragma acc atomic update
-            gy[k] -= dedy * redk;
-            #pragma acc atomic update
-            gz[k] -= dedz * redk;
-            #pragma acc atomic update
-            gx[kv] -= dedx * redkv;
-            #pragma acc atomic update
-            gy[kv] -= dedy * redkv;
-            #pragma acc atomic update
-            gz[kv] -= dedz * redkv;
-          }
-
-          if_constexpr(do_v) {
-            real vxx = xr * dedx;
-            real vyx = yr * dedx;
-            real vzx = zr * dedx;
-            real vyy = yr * dedy;
-            real vzy = zr * dedy;
-            real vzz = zr * dedz;
-
-            #pragma acc atomic update
-            vir_ev[_xx] += vxx;
-            #pragma acc atomic update
-            vir_ev[_yx] += vyx;
-            #pragma acc atomic update
-            vir_ev[_zx] += vzx;
-            #pragma acc atomic update
-            vir_ev[_xy] += vyx;
-            #pragma acc atomic update
-            vir_ev[_yy] += vyy;
-            #pragma acc atomic update
-            vir_ev[_zy] += vzy;
-            #pragma acc atomic update
-            vir_ev[_xz] += vzx;
-            #pragma acc atomic update
-            vir_ev[_yz] += vzy;
-            #pragma acc atomic update
-            vir_ev[_zz] += vzz;
-          }
-        }
-      } // end for (int kk)
-
-      #pragma acc loop independent
-      for (int j = 0; j < n12i; ++j)
-        vscale[couple->i12[i][j]] = 1;
-      #pragma acc loop independent
-      for (int j = 0; j < n13i; ++j)
-        vscale[couple->i13[i][j]] = 1;
-      #pragma acc loop independent
-      for (int j = 0; j < n14i; ++j)
-        vscale[couple->i14[i][j]] = 1;
-      #pragma acc loop independent
-      for (int j = 0; j < n15i; ++j)
-        vscale[couple->i15[i][j]] = 1;
-    } // end for (int i)
   }
+
+  #pragma acc parallel loop independent\
+              deviceptr(x,y,z,gx,gy,gz,box,couple,vlst,\
+                        ired,kred,xred,yred,zred,\
+                        jvdw,njvdw,radmin,epsilon,vlam,\
+                        ev,nev,vir_ev)\
+              firstprivate(vscale[0:n])
+  for (int i = 0; i < n; ++i) {
+    const int n12i = couple->n12[i];
+    const int n13i = couple->n13[i];
+    const int n14i = couple->n14[i];
+    const int n15i = couple->n15[i];
+    #pragma acc loop independent
+    for (int j = 0; j < n12i; ++j)
+      vscale[couple->i12[i][j]] = v2scale;
+    #pragma acc loop independent
+    for (int j = 0; j < n13i; ++j)
+      vscale[couple->i13[i][j]] = v3scale;
+    #pragma acc loop independent
+    for (int j = 0; j < n14i; ++j)
+      vscale[couple->i14[i][j]] = v4scale;
+    #pragma acc loop independent
+    for (int j = 0; j < n15i; ++j)
+      vscale[couple->i15[i][j]] = v5scale;
+
+    int iv = ired[i];
+    real redi = kred[i];
+    real rediv = 1 - redi;
+    int it = jvdw[i];
+    real xi, yi, zi;
+    xi = xred[i];
+    yi = yred[i];
+    zi = zred[i];
+    real lam1 = vlam[i];
+
+    int base_it = it * (*njvdw);
+    int nvlsti = vlst->nlst[i];
+    int base = i * maxnlst;
+    #pragma acc loop independent
+    for (int kk = 0; kk < nvlsti; ++kk) {
+      int k = vlst->lst[base + kk];
+      int kv = ired[k];
+      int kt = jvdw[k];
+      real xr, yr, zr;
+      xr = xi - xred[k];
+      yr = yi - yred[k];
+      zr = zi - zred[k];
+      real vlambda = vlam[k];
+
+      if (vcouple == vcouple_decouple) {
+        vlambda = (lam1 == vlambda ? 1 : (lam1 < vlambda ? lam1 : vlambda));
+      } else if (vcouple == vcouple_annihilate) {
+        vlambda = (lam1 < vlambda ? lam1 : vlambda);
+      }
+
+      image(xr, yr, zr, box);
+      real rik2 = xr * xr + yr * yr + zr * zr;
+      if (rik2 <= off2) {
+        real rik = REAL_SQRT(rik2);
+        real rv = radmin[base_it + kt];
+        real eps = epsilon[base_it + kt] * vscale[k];
+
+        real e;
+        real de;
+        if_constexpr(VDWTYP & evdw_hal) {
+          real rho = rik * REAL_RECIP(rv);
+          real rho6 = REAL_POW(rho, 6);
+          real rho7 = rho6 * rho;
+          eps *= REAL_POW(vlambda, scexp);
+          real scal = scalpha * REAL_POW(1 - vlambda, 2);
+          real s1 = REAL_RECIP(scal + REAL_POW(rho + dhal, 7));
+          real s2 = REAL_RECIP(scal + rho7 + ghal);
+          real t1 = REAL_POW(1 + dhal, 7) * s1;
+          real t2 = (1 + ghal) * s2;
+          e = eps * t1 * (t2 - 2);
+          if_constexpr(do_g) {
+            real dt1drho = -7 * REAL_POW(rho + dhal, 6) * t1 * s1;
+            real dt2drho = -7 * rho6 * t2 * s2;
+            de = eps * (dt1drho * (t2 - 2) + t1 * dt2drho) * REAL_RECIP(rv);
+          }
+        }
+
+        if (rik2 > cut2) {
+          real taper, dtaper;
+          switch_taper5<do_g>(rik, cut, off, taper, dtaper);
+          if_constexpr(do_g) de = e * dtaper + de * taper;
+          if_constexpr(do_e) e = e * taper;
+        }
+
+        // Increment the energy, gradient, and virial.
+
+        if_constexpr(do_e) {
+          #pragma acc atomic update
+          *ev += e;
+          if_constexpr(do_a) {
+            if (e != 0) {
+              #pragma acc atomic update
+              *nev += 1;
+            }
+          }
+        }
+
+        real dedx, dedy, dedz;
+        if_constexpr(do_g) {
+          de *= REAL_RECIP(rik);
+          dedx = de * xr;
+          dedy = de * yr;
+          dedz = de * zr;
+
+          #pragma acc atomic update
+          gx[i] += dedx * redi;
+          #pragma acc atomic update
+          gy[i] += dedy * redi;
+          #pragma acc atomic update
+          gz[i] += dedz * redi;
+          #pragma acc atomic update
+          gx[iv] += dedx * rediv;
+          #pragma acc atomic update
+          gy[iv] += dedy * rediv;
+          #pragma acc atomic update
+          gz[iv] += dedz * rediv;
+
+          real redk = kred[k];
+          real redkv = 1 - redk;
+          #pragma acc atomic update
+          gx[k] -= dedx * redk;
+          #pragma acc atomic update
+          gy[k] -= dedy * redk;
+          #pragma acc atomic update
+          gz[k] -= dedz * redk;
+          #pragma acc atomic update
+          gx[kv] -= dedx * redkv;
+          #pragma acc atomic update
+          gy[kv] -= dedy * redkv;
+          #pragma acc atomic update
+          gz[kv] -= dedz * redkv;
+        }
+
+        if_constexpr(do_v) {
+          real vxx = xr * dedx;
+          real vyx = yr * dedx;
+          real vzx = zr * dedx;
+          real vyy = yr * dedy;
+          real vzy = zr * dedy;
+          real vzz = zr * dedz;
+
+          #pragma acc atomic update
+          vir_ev[_xx] += vxx;
+          #pragma acc atomic update
+          vir_ev[_yx] += vyx;
+          #pragma acc atomic update
+          vir_ev[_zx] += vzx;
+          #pragma acc atomic update
+          vir_ev[_xy] += vyx;
+          #pragma acc atomic update
+          vir_ev[_yy] += vyy;
+          #pragma acc atomic update
+          vir_ev[_zy] += vzy;
+          #pragma acc atomic update
+          vir_ev[_xz] += vzx;
+          #pragma acc atomic update
+          vir_ev[_yz] += vzy;
+          #pragma acc atomic update
+          vir_ev[_zz] += vzz;
+        }
+      }
+    } // end for (int kk)
+
+    #pragma acc loop independent
+    for (int j = 0; j < n12i; ++j)
+      vscale[couple->i12[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < n13i; ++j)
+      vscale[couple->i13[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < n14i; ++j)
+      vscale[couple->i14[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < n15i; ++j)
+      vscale[couple->i15[i][j]] = 1;
+  } // end for (int i)
 }
 }
 TINKER_NAMESPACE_END
