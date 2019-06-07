489e6b774ad6c1a5264df1e9be4eff87ac76e1b5
Zhi Wang <oukore@gmail.com>
2019-06-07 14:29:40 -0500

    ufield (ewald version) passed the local-frame test case

--- src/gpu/acc_field_ewald.cc
+++ src/gpu/acc_field_ewald.cc
@@ -73,10 +73,10 @@ void dfield_ewald_real(real* gpu_field, real* gpu_fieldp) {
   real bn[4];
 
   #pragma acc parallel loop independent\
-                deviceptr(x,y,z,box,couple,polargroup,mlst,\
-                rpole,thole,pdamp,\
-                field,fieldp)\
-                firstprivate(pscale[0:n],dscale[0:n])
+              deviceptr(x,y,z,box,couple,polargroup,mlst,\
+              rpole,thole,pdamp,\
+              field,fieldp)\
+              firstprivate(pscale[0:n],dscale[0:n])
   for (int i = 0; i < n; ++i) {
 
     // set exclusion coefficients for connected atoms
@@ -129,21 +129,17 @@ void dfield_ewald_real(real* gpu_field, real* gpu_fieldp) {
     }
 
     #pragma acc loop independent
-    for (int j = 0; j < np11i; ++j) {
+    for (int j = 0; j < np11i; ++j)
       dscale[polargroup->ip11[i][j]] = d1scale;
-    }
     #pragma acc loop independent
-    for (int j = 0; j < np12i; ++j) {
+    for (int j = 0; j < np12i; ++j)
       dscale[polargroup->ip12[i][j]] = d2scale;
-    }
     #pragma acc loop independent
-    for (int j = 0; j < np13i; ++j) {
+    for (int j = 0; j < np13i; ++j)
       dscale[polargroup->ip13[i][j]] = d3scale;
-    }
     #pragma acc loop independent
-    for (int j = 0; j < np14i; ++j) {
+    for (int j = 0; j < np14i; ++j)
       dscale[polargroup->ip14[i][j]] = d4scale;
-    }
 
     real xi = x[i];
     real yi = y[i];
@@ -356,7 +352,7 @@ void ufield_ewald_recip_self(const real* gpu_uind, const real* gpu_uinp,
   const real aewald = st.aewald;
 
   cuind_to_fuind(pu, uind, uinp, fuind, fuinp);
-  grid_uind(pu, fuind, fuind);
+  grid_uind(pu, fuind, fuinp);
   fftfront(pu);
   // TODO: store vs. recompute qfrac
   pme_conv0(pu);
@@ -395,7 +391,188 @@ void ufield_ewald_recip_self(const real* gpu_uind, const real* gpu_uinp,
 }
 
 void ufield_ewald_real(const real* gpu_uind, const real* gpu_uinp,
-                       real* gpu_field, real* gpu_fieldp) {}
+                       real* gpu_field, real* gpu_fieldp) {
+  const real(*uind)[3] = reinterpret_cast<const real(*)[3]>(gpu_uind);
+  const real(*uinp)[3] = reinterpret_cast<const real(*)[3]>(gpu_uinp);
+  real(*field)[3] = reinterpret_cast<real(*)[3]>(gpu_field);
+  real(*fieldp)[3] = reinterpret_cast<real(*)[3]>(gpu_fieldp);
+
+  const real off = ewald_switch_cut;
+  const real off2 = off * off;
+  const int maxnlst = mlist_obj_.maxnlst;
+
+  const real u1scale = polpot::u1scale;
+  const real u2scale = polpot::u2scale;
+  const real u3scale = polpot::u3scale;
+  const real u4scale = polpot::u4scale;
+
+  static std::vector<real> uscalebuf;
+  uscalebuf.resize(n, 1);
+  real* uscale = uscalebuf.data();
+
+  const real aewald = pme_obj(ppme_unit).aewald;
+  const real aesq2 = 2 * aewald * aewald;
+  const real aesq2n = (aewald > 0 ? 1 / (sqrtpi * aewald) : 0);
+
+  real bn[3];
+
+  #pragma acc parallel loop independent\
+              deviceptr(x,y,z,box,polargroup,mlst,\
+              thole,pdamp,uind,uinp,field,fieldp)\
+              firstprivate(uscale[0:n])
+  for (int i = 0; i < n; ++i) {
+
+    // set exclusion coefficients for connected atoms
+
+    const int np11i = polargroup->np11[i];
+    const int np12i = polargroup->np12[i];
+    const int np13i = polargroup->np13[i];
+    const int np14i = polargroup->np14[i];
+
+    #pragma acc loop independent
+    for (int j = 0; j < np11i; ++j)
+      uscale[polargroup->ip11[i][j]] = u1scale;
+    #pragma acc loop independent
+    for (int j = 0; j < np12i; ++j)
+      uscale[polargroup->ip12[i][j]] = u2scale;
+    #pragma acc loop independent
+    for (int j = 0; j < np13i; ++j)
+      uscale[polargroup->ip13[i][j]] = u3scale;
+    #pragma acc loop independent
+    for (int j = 0; j < np14i; ++j)
+      uscale[polargroup->ip14[i][j]] = u4scale;
+
+    real xi = x[i];
+    real yi = y[i];
+    real zi = z[i];
+
+    int nmlsti = mlst->nlst[i];
+    int base = i * maxnlst;
+    #pragma acc loop independent private(bn[0:3])
+    for (int kk = 0; kk < nmlsti; ++kk) {
+      int k = mlst->lst[base + kk];
+      real xr = x[k] - xi;
+      real yr = y[k] - yi;
+      real zr = z[k] - zi;
+      real pdi = pdamp[i];
+      real pti = thole[i];
+
+      image(xr, yr, zr, box);
+      real r2 = xr * xr + yr * yr + zr * zr;
+      if (r2 <= off2) {
+        real r = REAL_SQRT(r2);
+        real rr1 = REAL_RECIP(r);
+        real rr2 = rr1 * rr1;
+        real rr3 = rr1 * rr2;
+        real rr5 = 3 * rr3 * rr2;
+
+        real ralpha = aewald * r;
+        bn[0] = REAL_ERFC(ralpha) * rr1;
+        real exp2a = REAL_EXP(-REAL_SQ(ralpha));
+        real aefac = aesq2n;
+        #pragma acc loop seq
+        for (int j = 1; j <= 2; ++j) {
+          aefac *= aesq2;
+          bn[j] = ((j + j - 1) * bn[j - 1] + aefac * exp2a) * rr2;
+        }
+
+        // if use_thole
+        real scale3 = 1;
+        real scale5 = 1;
+        real damp = pdi * pdamp[k];
+        if (damp != 0) {
+          real pgamma = REAL_MIN(pti, thole[k]);
+          damp = -pgamma * REAL_CUBE((r / damp));
+          if (damp > -50) {
+            real expdamp = REAL_EXP(damp);
+            scale3 = 1 - expdamp;
+            scale5 = 1 - expdamp * (1 - damp);
+          }
+        }
+        real scalek, bcn1, bcn2;
+
+        scalek = uscale[k];
+        bcn1 = bn[1] - (1 - scalek * scale3) * rr3;
+        bcn2 = bn[2] - (1 - scalek * scale5) * rr5;
+
+        real dlocal1 = bcn2 * xr * xr - bcn1;
+        real dlocal2 = bcn2 * xr * yr;
+        real dlocal3 = bcn2 * xr * zr;
+        real dlocal4 = bcn2 * yr * yr - bcn1;
+        real dlocal5 = bcn2 * yr * zr;
+        real dlocal6 = bcn2 * zr * zr - bcn1;
+
+        real fid1 =
+            dlocal1 * uind[k][0] + dlocal2 * uind[k][1] + dlocal3 * uind[k][2];
+        real fid2 =
+            dlocal2 * uind[k][0] + dlocal4 * uind[k][1] + dlocal5 * uind[k][2];
+        real fid3 =
+            dlocal3 * uind[k][0] + dlocal5 * uind[k][1] + dlocal6 * uind[k][2];
+        real fkd1 =
+            dlocal1 * uind[i][0] + dlocal2 * uind[i][1] + dlocal3 * uind[i][2];
+        real fkd2 =
+            dlocal2 * uind[i][0] + dlocal4 * uind[i][1] + dlocal5 * uind[i][2];
+        real fkd3 =
+            dlocal3 * uind[i][0] + dlocal5 * uind[i][1] + dlocal6 * uind[i][2];
+
+        real fip1 =
+            dlocal1 * uinp[k][0] + dlocal2 * uinp[k][1] + dlocal3 * uinp[k][2];
+        real fip2 =
+            dlocal2 * uinp[k][0] + dlocal4 * uinp[k][1] + dlocal5 * uinp[k][2];
+        real fip3 =
+            dlocal3 * uinp[k][0] + dlocal5 * uinp[k][1] + dlocal6 * uinp[k][2];
+        real fkp1 =
+            dlocal1 * uinp[i][0] + dlocal2 * uinp[i][1] + dlocal3 * uinp[i][2];
+        real fkp2 =
+            dlocal2 * uinp[i][0] + dlocal4 * uinp[i][1] + dlocal5 * uinp[i][2];
+        real fkp3 =
+            dlocal3 * uinp[i][0] + dlocal5 * uinp[i][1] + dlocal6 * uinp[i][2];
+
+        #pragma acc atomic update
+        field[i][0] += fid1;
+        #pragma acc atomic update
+        field[i][1] += fid2;
+        #pragma acc atomic update
+        field[i][2] += fid3;
+        #pragma acc atomic update
+        field[k][0] += fkd1;
+        #pragma acc atomic update
+        field[k][1] += fkd2;
+        #pragma acc atomic update
+        field[k][2] += fkd3;
+
+        #pragma acc atomic update
+        fieldp[i][0] += fip1;
+        #pragma acc atomic update
+        fieldp[i][1] += fip2;
+        #pragma acc atomic update
+        fieldp[i][2] += fip3;
+        #pragma acc atomic update
+        fieldp[k][0] += fkp1;
+        #pragma acc atomic update
+        fieldp[k][1] += fkp2;
+        #pragma acc atomic update
+        fieldp[k][2] += fkp3;
+        // end if use_thole
+      }
+    } // end for (int kk)
+
+    // reset exclusion coefficients for connected atoms
+
+    #pragma acc loop independent
+    for (int j = 0; j < np11i; ++j)
+      uscale[polargroup->ip11[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < np12i; ++j)
+      uscale[polargroup->ip12[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < np13i; ++j)
+      uscale[polargroup->ip13[i][j]] = 1;
+    #pragma acc loop independent
+    for (int j = 0; j < np14i; ++j)
+      uscale[polargroup->ip14[i][j]] = 1;
+  } // end for (int i)
+}
 
 void ufield_ewald(const real* gpu_uind, const real* gpu_uinp, real* gpu_field,
                   real* gpu_fieldp) {
