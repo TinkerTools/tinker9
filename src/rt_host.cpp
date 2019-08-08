123fa0d428d20a6e1022351086a233b816b93c9d
Zhi Wang <oukore@gmail.com>
2019-08-08 04:55:47 -0500

    compiling on macOS was re-enabled

--- src/rt_host.cpp
+++ src/rt_host.cpp
@@ -1,4 +1,5 @@
 #include "rt.h"
+#include <cstdlib>
 #include <cstring>
 #include <thread>
 
--- src/x_info.cpp
+++ src/x_info.cpp
@@ -1,10 +1,15 @@
 #include "tinker_rt.h"
 
 TINKER_NAMESPACE_BEGIN
+static const char* get_SHA1();
+
 void x_info(int argc, char** argv) {
   auto out = stdout;
 
   print(out, " {}\n\n", "Program Information");
+
+  print(out, "   > This Build Is Synchronized with Tinker of Commit :  {}\n",
+        get_SHA1());
 
 #ifdef TINKER_HOST
   auto loc = "Host";
@@ -15,5 +20,13 @@ void x_info(int argc, char** argv) {
 
   auto rs = sizeof(real);
   print(out, "   > Size of Real: {}\n", sizeof(real));
+}
+
+static const char* get_SHA1() {
+  return
+      // "36063480f115bd0455d4d6291f9666ab5f0b739d";
+      // "ddfb803a9a35237dd624a9eb9e74ddde4ea9062f";
+      // "904bc0125aa0ab548866bc3effed34df1ec1b4d6";
+      "6fe8e913fe4da3d46849d10248ad2a4872b4da93";
 }
 TINKER_NAMESPACE_END
