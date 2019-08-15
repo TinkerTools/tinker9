560f2d65dd470c421bff7373cbfb58a467cd884b
Zhi Wang <oukore@gmail.com>
2019-08-15 16:29:15 -0500

    updated analyze output

--- src/x_analyze.cpp
+++ src/x_analyze.cpp
@@ -2,6 +2,7 @@
 #include "io_print.h"
 #include "io_read.h"
 #include "md.h"
+#include "potent.h"
 #include "rc_man.h"
 #include "tinker_rt.h"
 
@@ -20,7 +21,6 @@ void x_analyze(int argc, char** argv) {
   if (exist) {
     read_string(opt, string);
   }
-  Text::upcase(opt);
   //   std::string prompt = R"(
   //  The Tinker Energy Analysis Utility Can :
 
@@ -40,7 +40,7 @@ void x_analyze(int argc, char** argv) {
 
  Total Potential Energy and its Components [E]
 
- Enter the Desired Analysis Types [G,P,E,A,L,D,M,V,C] :  )";
+ Enter the Desired Analysis Types [E] :  )";
   read_stream(opt, prompt, std::string("#"), [](std::string s) {
     Text::upcase(s);
     if (s == "E")
@@ -49,6 +49,7 @@ void x_analyze(int argc, char** argv) {
       return true;
   });
 
+  Text::upcase(opt);
   if (opt == "E")
     option_e();
 
@@ -67,6 +68,13 @@ static void option_e() {
   double sum = get_energy(esum);
   auto& out = std::cout;
   print(out, "\n Total Potential Energy :        {:16.4f} Kcal/mole\n", sum);
+  print(out,
+        "\n Energy Component Breakdown :           Kcal/mole        "
+        "Interactions\n\n");
+
+  const char* fmt = " {:<29s}{:19.4f}{:17d}\n";
+  if (use_potent(polar_term))
+    print(out, fmt, "Polarization", get_energy(ep), get_count(nep));
 
   finish();
 }
