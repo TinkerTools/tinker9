c0a12a1d4526f21ec705ce8bb8d3f81e5f874466
Zhi Wang <oukore@gmail.com>
2019-09-18 00:52:53 -0500

    save the progress

---
+++ make/macos.acc.clang++.xcode.10.1.mk
@@ -0,0 +1,9 @@
+ACC := $(CXX)
+acc_depend_flags__ := $(cxx_depend_flags__)
+acc_compile_flags__ := $(cxx_compile_flags__)
+
+$(acc_dependency__): %.d: $(top_dir__)/%.cpp
+	printf %s $(@D)/ > $@
+	$(ACC) $< $(acc_depend_flags__) >> $@
+$(all_acc_objs__): %.o: $(top_dir__)/%.cpp
+	$(ACC) $< $(acc_compile_flags__) -o $@
