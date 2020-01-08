e46f83ef36b8978108377073a6cf141b98e7dd89
Zhi Wang <oukore@gmail.com>
2020-01-08 07:47:49 -0600

    Added cc70 flag

--- make/linux.acc.device.pgc++.mk
+++ make/linux.acc.device.pgc++.mk
@@ -1,11 +1,11 @@
 ACC := pgc++ CUDA_HOME=$(cuda_dir)
-acc_opt_flags__ := -Mcuda=cc35,cc60
+acc_opt_flags__ := -Mcuda=cc35,cc60,cc70
 ifeq ($(opt),debug)
-   acc_opt_flags__ += -O0 -g -traceback -ta=tesla:cc35,cc60
+   acc_opt_flags__ += -O0 -g -traceback -ta=tesla:cc35,cc60,cc70,lineinfo
 else ifeq ($(opt),release)
-   acc_opt_flags__ += -O3 -ta=tesla:fastmath,cc35,cc60
+   acc_opt_flags__ += -O3 -ta=tesla:fastmath,cc35,cc60,cc70
 else ifeq ($(opt),profile)
-   acc_opt_flags__ += -O3 -g -ta=tesla:fastmath,cc35,cc60
+   acc_opt_flags__ += -O3 -g -ta=tesla:fastmath,cc35,cc60,cc70,lineinfo
 endif
 acc_flags__ := -std=c++11 -acc verystrict -Minfo=accel $(shared_flags__) $(acc_opt_flags__)
 
