c7a81d2930d8241e2ddbe0a6b8b55a8103fcd442
Zhi Wang <oukore@gmail.com>
2020-10-06 03:54:30 -0500

    dfield now uses spatial2

--- include/seq_pair_field.h
+++ include/seq_pair_field.h
@@ -97,6 +97,112 @@ void pair_dfield(real r2, real xr, real yr, real zr, real dscale,
 }
 
 
+#pragma acc routine seq
+template <class ETYP>
+SEQ_CUDA
+void pair_dfield_v2(real r2, real xr, real yr, real zr, real dscale,
+                    real pscale, real aewald, //
+                    real ci, real dix, real diy, real diz, real qixx, real qixy,
+                    real qixz, real qiyy, real qiyz, real qizz, real pdi,
+                    real pti, //
+                    real ck, real dkx, real dky, real dkz, real qkxx, real qkxy,
+                    real qkxz, real qkyy, real qkyz, real qkzz, real pdk,
+                    real ptk, //
+                    real& restrict fidx, real& restrict fidy,
+                    real& restrict fidz, real& restrict fipx,
+                    real& restrict fipy, real& restrict fipz,
+                    real& restrict fkdx, real& restrict fkdy,
+                    real& restrict fkdz, real& restrict fkpx,
+                    real& restrict fkpy, real& restrict fkpz)
+{
+   real r = REAL_SQRT(r2);
+   real invr1 = REAL_RECIP(r);
+   real rr2 = invr1 * invr1;
+
+   real scale3, scale5, scale7;
+   damp_thole3(r, pdi, pti, pdk, ptk, scale3, scale5, scale7);
+
+   real bn[4];
+   if CONSTEXPR (eq<ETYP, EWALD>())
+      damp_ewald<4>(bn, r, invr1, rr2, aewald);
+   real rr1 = invr1;
+   real rr3 = rr1 * rr2;
+   real rr5 = 3 * rr1 * rr2 * rr2;
+   real rr7 = 15 * rr1 * rr2 * rr2 * rr2;
+
+   real dir = dix * xr + diy * yr + diz * zr;
+   real qix = qixx * xr + qixy * yr + qixz * zr;
+   real qiy = qixy * xr + qiyy * yr + qiyz * zr;
+   real qiz = qixz * xr + qiyz * yr + qizz * zr;
+   real qir = qix * xr + qiy * yr + qiz * zr;
+   real dkr = dkx * xr + dky * yr + dkz * zr;
+   real qkx = qkxx * xr + qkxy * yr + qkxz * zr;
+   real qky = qkxy * xr + qkyy * yr + qkyz * zr;
+   real qkz = qkxz * xr + qkyz * yr + qkzz * zr;
+   real qkr = qkx * xr + qky * yr + qkz * zr;
+
+   real3 dixyz = make_real3(dix, diy, diz);
+   real3 dkxyz = make_real3(dkx, dky, dkz);
+   real3 qixyz = make_real3(qix, qiy, qiz);
+   real3 qkxyz = make_real3(qkx, qky, qkz);
+   real3 dr = make_real3(xr, yr, zr);
+   real c1;
+   real3 inci, inck;
+
+   // d-field
+   real bn1, bn2, bn3;
+   if CONSTEXPR (eq<ETYP, EWALD>()) {
+      bn1 = bn[1];
+      bn2 = bn[2];
+      bn3 = bn[3];
+      bn[1] = bn1 - (1 - dscale * scale3) * rr3;
+      bn[2] = bn2 - (1 - dscale * scale5) * rr5;
+      bn[3] = bn3 - (1 - dscale * scale7) * rr7;
+   } else if CONSTEXPR (eq<ETYP, NON_EWALD>()) {
+      bn[1] = dscale * scale3 * rr3;
+      bn[2] = dscale * scale5 * rr5;
+      bn[3] = dscale * scale7 * rr7;
+   }
+
+   c1 = -(bn[1] * ck - bn[2] * dkr + bn[3] * qkr);
+   inci = c1 * dr - bn[1] * dkxyz + 2 * bn[2] * qkxyz;
+   fidx += inci.x;
+   fidy += inci.y;
+   fidz += inci.z;
+
+   c1 = (bn[1] * ci + bn[2] * dir + bn[3] * qir);
+   inck = c1 * dr - bn[1] * dixyz - 2 * bn[2] * qixyz;
+   fkdx += inck.x;
+   fkdy += inck.y;
+   fkdz += inck.z;
+
+   // p-field
+   if (pscale != dscale) {
+      if CONSTEXPR (eq<ETYP, EWALD>()) {
+         bn[1] = bn1 - (1 - pscale * scale3) * rr3;
+         bn[2] = bn2 - (1 - pscale * scale5) * rr5;
+         bn[3] = bn3 - (1 - pscale * scale7) * rr7;
+      } else if CONSTEXPR (eq<ETYP, NON_EWALD>()) {
+         bn[1] = pscale * scale3 * rr3;
+         bn[2] = pscale * scale5 * rr5;
+         bn[3] = pscale * scale7 * rr7;
+      }
+
+      c1 = -(bn[1] * ck - bn[2] * dkr + bn[3] * qkr);
+      inci = c1 * dr - bn[1] * dkxyz + 2 * bn[2] * qkxyz;
+
+      c1 = (bn[1] * ci + bn[2] * dir + bn[3] * qir);
+      inck = c1 * dr - bn[1] * dixyz - 2 * bn[2] * qixyz;
+   }
+   fipx += inci.x;
+   fipy += inci.y;
+   fipz += inci.z;
+   fkpx += inck.x;
+   fkpy += inck.y;
+   fkpz += inck.z;
+}
+
+
 template <class ETYP>
 SEQ_CUDA
 void pair_ufield(real r2, real xr, real yr, real zr, real uscale, //
