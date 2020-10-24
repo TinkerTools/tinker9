dcbdbde27039d42ad189169904e7dc9518ad2ab4
Zhi Wang <oukore@gmail.com>
2020-10-24 02:08:27 -0500

    mute c++11 warning in thrust 1.9.9

--- src/testgrad_x.cpp
+++ src/testgrad_x.cpp
@@ -65,16 +65,16 @@ void x_testgrad(int, char**)
       fmt = "\n Anlyt%10d       %12.4f%12.4f%12.4f  %12.4f";
    }
    print(out, fmt_t, "");
-   auto do_print = [](int i, int n, int top_m) {
-      if (n <= 2 * top_m)
-         return true;
-      else if (i < top_m)
-         return true;
-      else if (i >= n - top_m)
-         return true;
-      else
-         return false;
-   };
+   // auto do_print = [](int i, int n, int top_m) {
+   //    if (n <= 2 * top_m)
+   //       return true;
+   //    else if (i < top_m)
+   //       return true;
+   //    else if (i >= n - top_m)
+   //       return true;
+   //    else
+   //       return false;
+   // };
    // int print_top_n = 15;
    for (int i = 0; i < n; ++i) {
       // if (not do_print(i, n, print_top_n))
