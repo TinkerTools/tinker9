630e370821263ee011e0f33833c048415df9a009
Zhi Wang <oukore@gmail.com>
2020-11-25 21:49:26 -0600

    detect clang-format version

--- code-format.sh
+++ code-format.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+DETECTED=$(clang-format --version)            # Ubuntu clang-format version 10.0.1-++20200708123514+ef32c611aa2-1~exp1~20200707224111.189
+DETECTED="${DETECTED##*clang-format version}" # 10.0.1-++20200708123514+ef32c611aa2-1~exp1~20200707224111.189
+DETECTED="${DETECTED%%.*}"                    # 10
+if [ $DETECTED -lt 10 ]; then
+   echo Must use clang-format version 10.0.0+
+   exit 1
+fi
+
 OS=$(uname -s)
 if   [ $OS == Linux  ]; then
    SED='sed -i'
