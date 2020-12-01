fe4d6872223b9037c244ac5c7b6fe99bc1332373
Zhi Wang <oukore@gmail.com>
2020-12-01 17:03:56 -0600

    update eurey test

--- src/eurey_acc.cpp
+++ src/eurey_acc.cpp
@@ -1,13 +1,13 @@
 #include "add.h"
 #include "eurey.h"
 #include "md.h"
+#include "seq_urey.h"
 
 namespace tinker {
 template <class Ver>
 void eurey_acc1()
 {
    constexpr bool do_e = Ver::e;
-   constexpr bool do_g = Ver::g;
    constexpr bool do_v = Ver::v;
 
    auto bufsize = buffer_size();
@@ -18,50 +18,16 @@ void eurey_acc1()
                eub,vir_eub)
    for (int i = 0; i < nurey; ++i) {
       int offset = i & (bufsize - 1);
-      const int ia = iury[i][0];
-      const int ic = iury[i][2];
-      const real ideal = ul[i];
-      const real force = uk[i];
+      real e, vxx, vyx, vzx, vyy, vzy, vzz;
+      dk_urey<Ver>(e, vxx, vyx, vzx, vyy, vzy, vzz,
 
-      real xac = x[ia] - x[ic];
-      real yac = y[ia] - y[ic];
-      real zac = z[ia] - z[ic];
+                   deubx, deuby, deubz,
 
-      real rac = REAL_SQRT(xac * xac + yac * yac + zac * zac);
-      real dt = rac - ideal;
-      real dt2 = dt * dt;
-
-      if CONSTEXPR (do_e) {
-         real e = ureyunit * force * dt2 * (1 + cury * dt + qury * dt2);
+                   ureyunit, i, iury, uk, ul, cury, qury, x, y, z);
+      if CONSTEXPR (do_e)
          atomic_add(e, eub, offset);
-      }
-
-      if CONSTEXPR (do_g) {
-         real deddt =
-            2 * ureyunit * force * dt * (1 + 1.5f * cury * dt + 2 * qury * dt2);
-         real de = deddt * REAL_RECIP(rac);
-         real dedx = de * xac;
-         real dedy = de * yac;
-         real dedz = de * zac;
-
-         atomic_add(dedx, deubx, ia);
-         atomic_add(dedy, deuby, ia);
-         atomic_add(dedz, deubz, ia);
-         atomic_add(-dedx, deubx, ic);
-         atomic_add(-dedy, deuby, ic);
-         atomic_add(-dedz, deubz, ic);
-
-         if CONSTEXPR (do_v) {
-            real vxx = xac * dedx;
-            real vyx = yac * dedx;
-            real vzx = zac * dedx;
-            real vyy = yac * dedy;
-            real vzy = zac * dedy;
-            real vzz = zac * dedz;
-
-            atomic_add(vxx, vyx, vzx, vyy, vzy, vzz, vir_eub, offset);
-         }
-      }
+      if CONSTEXPR (do_v)
+         atomic_add(vxx, vyx, vzx, vyy, vzy, vzz, vir_eub, offset);
    } // end for (int i)
 }
 
