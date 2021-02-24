33ea61199a2a3bafcfe0204f3c054ca756a6170d
Zhi Wang <oukore@gmail.com>
2021-02-24 15:54:52 -0600

    update docs for the launch functions

--- include/syntax/cu/launch.h
+++ include/syntax/cu/launch.h
@@ -15,10 +15,32 @@ namespace tinker {
  * \param np  Hint to determine the grid dimension (often is the loop limit).
  * \param k   CUDA `__global__` kernel.
  * \param a   Arguments of the kernel.
+ * \see launch_k4b
  */
 template <class K, class... Ts>
 void launch_k4(cudaStream_t st, size_t sh, int bs, int np, K k, Ts&&... a)
 {
+   int gs = (np + bs - 1) / bs;
+   k<<<gs, bs, sh, st>>>(std::forward<Ts>(a)...);
+}
+
+
+/**
+ * \ingroup cuda_syntax
+ * Launch a non-blocking CUDA kernel with energy, virial, and/or count buffer
+ * parameters.
+ * \param st  CUDA stream.
+ * \param sh  Dynamic shared memory (bytes per block).
+ * \param bs  Thread block size.
+ * \param np  Hint to determine the grid dimension (often is the loop limit).
+ * \param k   CUDA `__global__` kernel.
+ * \param a   Arguments of the kernel.
+ * \see launch_k4
+ */
+template <class K, class... Ts>
+void launch_k4b(cudaStream_t st, size_t sh, int bs, int np, K k, Ts&&... a)
+{
+   np = std::min(np, nelem_buffer);
    int gs = (np + bs - 1) / bs;
    k<<<gs, bs, sh, st>>>(std::forward<Ts>(a)...);
 }
@@ -35,12 +57,18 @@ void launch_k2s(cudaStream_t st, int bs, int np, K k, Ts&&... a)
    const size_t sh = 0;
    launch_k4(st, sh, bs, np, k, std::forward<Ts>(a)...);
 }
+
+
+/**
+ * \ingroup cuda_syntax
+ * Launch a non-blocking CUDA kernel with 0 dynamic shared memory.
+ * \see launch_k4b
+ */
 template <class K, class... Ts>
 void launch_k2b(cudaStream_t st, int bs, int np, K k, Ts&&... a)
 {
    const size_t sh = 0;
-   np = std::min(np, nelem_buffer);
-   launch_k4(st, sh, bs, np, k, std::forward<Ts>(a)...);
+   launch_k4b(st, sh, bs, np, k, std::forward<Ts>(a)...);
 }
 
 
@@ -56,6 +84,14 @@ void launch_k1s(cudaStream_t st, int np, K k, Ts&&... a)
    const int bs = BLOCK_DIM;
    launch_k2s(st, bs, np, k, std::forward<Ts>(a)...);
 }
+
+
+/**
+ * \ingroup cuda_syntax
+ * Launch a non-blocking CUDA kernel with 0 dynamic shared memory and the
+ * default block size (#BLOCK_DIM).
+ * \see BLOCK_DIM, launch_k2b, launch_k4b
+ */
 template <class K, class... Ts>
 void launch_k1b(cudaStream_t st, int np, K k, Ts&&... a)
 {
