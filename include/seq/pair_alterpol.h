3784a1c5c7640c800f283cf01511a077a5b9fef3
Zhi Wang <oukore@gmail.com>
2022-08-12 16:02:51 -0500

    expol: add screening S2 and G

--- include/seq/pair_alterpol.h
+++ include/seq/pair_alterpol.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ff/hippo/expolscr.h"
+#include "math/sinhc.h"
 #include "math/switch.h"
 #include "seq/seq.h"
 
@@ -13,9 +14,8 @@ inline void damp_expl(ExpolScr scrtyp, real& restrict s2, real& restrict ds2, re
    constexpr real inv2 = 1. / 2, inv3 = 1. / 3;
    constexpr real one = 1.;
 
-   real alphaik, dmpik2, dampik, dampik2, expik, s;
-
    if (scrtyp == ExpolScr::S2U) {
+      real alphaik, dmpik2, dampik, dampik2, expik, s;
       alphaik = REAL_SQRT(alphai * alphak);
       dmpik2 = inv2 * alphaik;
       dampik = dmpik2 * r;
@@ -24,6 +24,42 @@ inline void damp_expl(ExpolScr scrtyp, real& restrict s2, real& restrict ds2, re
       s = (one + dampik + dampik2 * inv3) * expik;
       s2 = s * s;
       if (DO_G) ds2 = s * (-alphaik * inv3) * (dampik + dampik2) * expik;
+   } else if (scrtyp == ExpolScr::S2) {
+      real pfac = 2 / (alphai + alphak);
+      real r2 = r * r;
+      pfac = pfac * pfac;
+      pfac = pfac * alphai * alphak;
+      pfac = pfac * pfac * pfac;
+      pfac *= r2;
+
+      real a = alphai * r / 2, b = alphak * r / 2;
+      real c = (a + b) / 2, d = (b - a) / 2;
+      real expmc = REAL_EXP(-c);
+
+      real c2 = c * c;
+      real d2 = d * d;
+      real c2d2 = (c * d) * (c * d);
+      real f1d, f2d, f3d;
+      fsinhc3(d, f1d, f2d, f3d);
+
+      real s;
+      s = f1d * (c + 1) + f2d * c2;
+      s /= r;
+      s *= expmc;
+      s2 = pfac * s * s;
+
+      if (DO_G) {
+         real ds;
+         ds = f1d * c2 + f2d * ((c - 2) * c2 - (c + 1) * d2) - f3d * c2d2;
+         ds /= -r2;
+         ds *= expmc;
+         ds2 = pfac * 2 * s * ds;
+      }
+
+   } else if (scrtyp == ExpolScr::G) {
+      real alphaik = REAL_SQRT(alphai * alphak);
+      s2 = REAL_EXP(-alphaik / (real)10 * r * r);
+      if (DO_G) ds2 = (-alphaik / (real)5) * r * s2;
    }
 
    s2 = sizik * s2;
@@ -92,7 +128,7 @@ inline void pair_dexpol(ExpolScr scrtyp, real r, real pscale, real cut, real off
    real ds2k = springk * ds2 * pscale;
 
    // compute rotation matrix
-   real ai[3][3], ak[3][3];
+   real ai[3][3];
    ai[0][2] = xr / r;
    ai[1][2] = yr / r;
    ai[2][2] = zr / r;
@@ -116,24 +152,16 @@ inline void pair_dexpol(ExpolScr scrtyp, real r, real pscale, real cut, real off
    ai[0][1] = ai[2][0] * ai[1][2] - ai[1][0] * ai[2][2];
    ai[1][1] = ai[0][0] * ai[2][2] - ai[2][0] * ai[0][2];
    ai[2][1] = ai[1][0] * ai[0][2] - ai[0][0] * ai[1][2];
-   ak[0][0] = ai[0][0];
-   ak[1][0] = ai[1][0];
-   ak[2][0] = ai[2][0];
-   ak[0][1] = -ai[0][1];
-   ak[1][1] = -ai[1][1];
-   ak[2][1] = -ai[2][1];
-   ak[0][2] = -ai[0][2];
-   ak[1][2] = -ai[1][2];
-   ak[2][2] = -ai[2][2];
+   // ak[][0] = ai[][0], ak[][1] = -ai[][1], ak[][2] = -ai[][2]
 
    // local frame force
    real frcil[3], frckl[3];
    real uixl = uix * ai[0][0] + uiy * ai[1][0] + uiz * ai[2][0];
    real uiyl = uix * ai[0][1] + uiy * ai[1][1] + uiz * ai[2][1];
    real uizl = uix * ai[0][2] + uiy * ai[1][2] + uiz * ai[2][2];
-   real ukxl = -(ukx * ak[0][0] + uky * ak[1][0] + ukz * ak[2][0]);
-   real ukyl = -(ukx * ak[0][1] + uky * ak[1][1] + ukz * ak[2][1]);
-   real ukzl = -(ukx * ak[0][2] + uky * ak[1][2] + ukz * ak[2][2]);
+   real ukxl = -(ukx * ai[0][0] + uky * ai[1][0] + ukz * ai[2][0]);
+   real ukyl = ukx * ai[0][1] + uky * ai[1][1] + ukz * ai[2][1];
+   real ukzl = ukx * ai[0][2] + uky * ai[1][2] + ukz * ai[2][2];
    frcil[2] = uizl * uizl * ds2i;
    frckl[2] = ukzl * ukzl * ds2k;
    // local frame torque
@@ -151,9 +179,9 @@ inline void pair_dexpol(ExpolScr scrtyp, real r, real pscale, real cut, real off
    real frcxi = ai[0][0] * frcil[0] + ai[0][1] * frcil[1] + ai[0][2] * frcil[2];
    real frcyi = ai[1][0] * frcil[0] + ai[1][1] * frcil[1] + ai[1][2] * frcil[2];
    real frczi = ai[2][0] * frcil[0] + ai[2][1] * frcil[1] + ai[2][2] * frcil[2];
-   real frcxk = ak[0][0] * frckl[0] + ak[0][1] * frckl[1] + ak[0][2] * frckl[2];
-   real frcyk = ak[1][0] * frckl[0] + ak[1][1] * frckl[1] + ak[1][2] * frckl[2];
-   real frczk = ak[2][0] * frckl[0] + ak[2][1] * frckl[1] + ak[2][2] * frckl[2];
+   real frcxk = ai[0][0] * frckl[0] - ai[0][1] * frckl[1] - ai[0][2] * frckl[2];
+   real frcyk = ai[1][0] * frckl[0] - ai[1][1] * frckl[1] - ai[1][2] * frckl[2];
+   real frczk = ai[2][0] * frckl[0] - ai[2][1] * frckl[1] - ai[2][2] * frckl[2];
    frc[0] = f * (frcxk - frcxi);
    frc[1] = f * (frcyk - frcyi);
    frc[2] = f * (frczk - frczi);
--- test/expol.cpp
+++ test/expol.cpp
@@ -228,3 +228,121 @@ TEST_CASE("ExchangePolarization-4", "[ff][hippo][expol]")
    finish();
    testEnd();
 }
+
+TEST_CASE("ExchangePolarization-5", "[ff][hippo][expol]")
+{
+   const char* xn = "crys.xyz";
+   const char* kn = "expols2.key";
+   const char* argv[] = {"dummy", xn, "-k", kn};
+   int argc = 4;
+
+   TestFile fx1(TINKER9_DIRSTR "/test/file/expol/crys.xyz");
+   TestFile fk1(TINKER9_DIRSTR "/test/file/expol/expol.key", kn,
+      "\n"
+      "exchange-polar s2"
+      "\n");
+   TestFile fp1(TINKER9_DIRSTR "/test/file/expol/expol.prm");
+
+   const double eps_e = testGetEps(0.0001, 0.0001);
+   const double eps_g = testGetEps(0.0001, 0.0001);
+   const double eps_v = testGetEps(0.001, 0.001);
+
+   TestReference r(TINKER9_DIRSTR "/test/ref/expol.5.txt");
+   auto ref_c = r.getCount();
+   auto ref_e = r.getEnergy();
+   auto ref_v = r.getVirial();
+   auto ref_g = r.getGradient();
+
+   rc_flag = calc::xyz | calc::vmask;
+   testBeginWithArgs(argc, argv);
+   initialize();
+
+   energy(calc::v0);
+   COMPARE_REALS(esum, ref_e, eps_e);
+
+   energy(calc::v1);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_GRADIENT(ref_g, eps_g);
+   for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+         COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_v);
+
+   energy(calc::v3);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_INTS(countReduce(nep), ref_c);
+
+   energy(calc::v4);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_GRADIENT(ref_g, eps_g);
+
+   energy(calc::v5);
+   COMPARE_GRADIENT(ref_g, eps_g);
+
+   energy(calc::v6);
+   COMPARE_GRADIENT(ref_g, eps_g);
+   for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+         COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_v);
+
+   finish();
+   testEnd();
+}
+
+TEST_CASE("ExchangePolarization-6", "[ff][hippo][expol]")
+{
+   const char* xn = "crys.xyz";
+   const char* kn = "expolg.key";
+   const char* argv[] = {"dummy", xn, "-k", kn};
+   int argc = 4;
+
+   TestFile fx1(TINKER9_DIRSTR "/test/file/expol/crys.xyz");
+   TestFile fk1(TINKER9_DIRSTR "/test/file/expol/expol.key", kn,
+      "\n"
+      "exchange-polar g"
+      "\n");
+   TestFile fp1(TINKER9_DIRSTR "/test/file/expol/expol.prm");
+
+   const double eps_e = testGetEps(0.0001, 0.0001);
+   const double eps_g = testGetEps(0.0001, 0.0001);
+   const double eps_v = testGetEps(0.001, 0.001);
+
+   TestReference r(TINKER9_DIRSTR "/test/ref/expol.6.txt");
+   auto ref_c = r.getCount();
+   auto ref_e = r.getEnergy();
+   auto ref_v = r.getVirial();
+   auto ref_g = r.getGradient();
+
+   rc_flag = calc::xyz | calc::vmask;
+   testBeginWithArgs(argc, argv);
+   initialize();
+
+   energy(calc::v0);
+   COMPARE_REALS(esum, ref_e, eps_e);
+
+   energy(calc::v1);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_GRADIENT(ref_g, eps_g);
+   for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+         COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_v);
+
+   energy(calc::v3);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_INTS(countReduce(nep), ref_c);
+
+   energy(calc::v4);
+   COMPARE_REALS(esum, ref_e, eps_e);
+   COMPARE_GRADIENT(ref_g, eps_g);
+
+   energy(calc::v5);
+   COMPARE_GRADIENT(ref_g, eps_g);
+
+   energy(calc::v6);
+   COMPARE_GRADIENT(ref_g, eps_g);
+   for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+         COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_v);
+
+   finish();
+   testEnd();
+}
