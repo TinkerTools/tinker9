75310d9c4731278779f63890cc66b1c0973caf1c
Zhi Wang <oukore@gmail.com>
2022-10-14 03:39:07 -0500

    update aplus field cu kernels

--- ext/ext/y3/genall.sh
+++ ext/ext/y3/genall.sh
@@ -3,5 +3,5 @@
 # Run a python script for all YAML files in this directory to generate their cuda kernals
 
 for f in *.yaml; do
-    python3 ../ck.py -c $f | bash
+    python3 ../ck3.py -c $f | bash
 done
